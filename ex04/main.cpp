18fa9b7361beca55f5aca2da0e308cd7544dbe4f
aklein <aklein@student.hive.fi>
2024-06-24 14:41:58 +0300

    sed usage print

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,7 +3,10 @@
 int	main(int argc, char **argv)
 {
 	if (argc != 4)
+	{
+		std::cout << "usage ./sed_losers <filename> <find> <replace>";
 		return 1;
+	}
 	Sed sed(argv[1], argv[2], argv[3]);
 	sed.run();
 }
